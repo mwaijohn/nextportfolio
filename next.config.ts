1253e99bb3b4e3e4cc8e6fbafb48c70d55fad279
John Mwai <mwaijohng@gmail.com>
2025-03-25 17:41:13 +0300

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Avoids Image Optimization issues on GitHub Pages
+  },
+  basePath: "nextportfolio", // Change this to your repo name
+  assetPrefix: "nextportfolio",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
