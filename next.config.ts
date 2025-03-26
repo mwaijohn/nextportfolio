41ca4edab4714021127d7023b59da3140298ef0b
john mwai <mwaijohng@gmail.com>
2025-03-26 08:39:09 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Enables static export
-  images: {
-    unoptimized: true, // Avoids Image Optimization issues on GitHub Pages
-  },
-  basePath: "/nextportfolio", // Change this to your repo name
-  assetPrefix: "/nextportfolio",
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export", // Enables static export
+//   images: {
+//     unoptimized: true, // Avoids Image Optimization issues on GitHub Pages
+//   },
+//   basePath: "/nextportfolio", // Change this to your repo name
+//   assetPrefix: "/nextportfolio",
+// };
 
 module.exports = nextConfig;
