a5edaabbf5268b7f57170d56fbbd0a7ba189aad7
John Mwai <mwaijohng@gmail.com>
2025-03-27 17:30:41 +0300

    header

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Header = () => {
     return (
-        <nav className="flex justify-between items-center py-4 px-6 md:px-12 font-semibold text-sm uppercase tracking-wide w-1/2 mx-auto">
+        <nav className="flex justify-between items-center py-4 px-6 md:px-12 font-semibold text-sm uppercase tracking-wide w-full lg:w-1/2 mx-auto">
             <Link href="#home" className="hover:text-green-600 transition">Home</Link>
             <Link href="#experience" className="hover:text-green-600 transition">Experience</Link>
             <Link href="#contacts" className="hover:text-green-600 transition">Contacts</Link>
